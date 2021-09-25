4cfd83a15a9691a7e625de469b5f22df42c9199d
Yuri <snicksnk@gmail.com>
2021-09-25 23:39:56 +0400

    Fixed example templates

--- examples/templates/ReactPage/__PageName__Page.tsx
+++ examples/templates/ReactPage/__PageName__Page.tsx
@@ -2,6 +2,6 @@ import /*=~ it.PageName */ from ".//*=~ it.PageName */"
 
 export default function /*=~ it.PageName */Page() {
   return (
-    </*=~ it.PageName */>
+    </*=~ it.PageName *//>
   );
 }
