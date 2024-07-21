44873e5f00f3e44dc51d79c6953aad9a496a8b9a
Yuri Taratorin <taratorkin@axlebolt.ltd>
2024-07-22 00:50:15 +0700

    fixed example template fixed processing params

--- src/bin.ts
+++ src/bin.ts
@@ -3,31 +3,59 @@ import * as fs from 'fs'
 import * as path from 'path'
 import * as chalk from 'chalk'
 
-import { getArgs, askFor, getArgValue } from './cli'
+import { getArgs, askFor } from './cli'
 import { ScenarioFunction } from './types'
 
 async function run () {
   const args = getArgs()
 
-  const scenariosDir = args.d || '__morphogen/scenarios'
+  const argv = require('yargs/yargs')(process.argv.slice(2))
+    .usage(`${chalk.greenBright('morphogen')} codegeneratrion tool`)
+    .help('help').alias('help', 'h')
+    .options({
+      scenariosDir: {
+        alias: 'd',
+        description: 'scenarios directory',
+        requiresArg: true,
+        required: true,
+        default: '__morphogen/scenarios'
+      },
+      scenarioName: {
+        alias: 's',
+        description: 'scenario name to run',
+        requiresArg: true,
+        required: false
+      }
+    })
+    .parse()
 
-  if (fs.existsSync(scenariosDir) === false) {
-    console.error(chalk.red(`Scenarios dir "${chalk.bold(scenariosDir)}" not found`))
+  if (fs.existsSync(argv.scenariosDir) === false) {
+    throw new Error(chalk.red(`Scenarios dir "${chalk.bold(argv.scenariosDir)}" not found`))
+  }
+
+  const elements: Array<string> = fs.readdirSync(argv.scenariosDir)
+
+  console.log(chalk.bgGray('Available scenarios: [scenarioName] full-path'))
+  if (!argv.scenarioName) {
+    elements.forEach((s) => {
+      console.log(`-s ${s.split('.').slice(0, -1).join('.')} ${chalk.green(path.join(argv.scenariosDir, s))}`)
+    })
     return
   }
 
-  const elements: Array<string> = fs.readdirSync(scenariosDir)
+  const scenarioPath = elements.find((s) => s.split('.').slice(0, -1).join('.') === argv.scenarioName)
 
-  elements.forEach((s, n) => {
-    console.log(`${chalk.grey(`[${n}]`)} ${chalk.green(s)}`)
-  })
-
-  const scenarioN = await askFor<keyof typeof elements>('Select scenario')
-  if (!elements?.[scenarioN]) {
-    throw new Error('Wrong scenario number')
+  if (!scenarioPath) {
+    throw new Error(`Scenario "${argv.scenarioName}" not found`)
   }
 
-  const scenarioFunction = require(path.join(process.cwd(), scenariosDir, elements[scenarioN as number])) as ScenarioFunction
+  const pathOfScenario = path.join(process.cwd(), argv.scenariosDir, scenarioPath)
+
+  if (fs.existsSync(pathOfScenario) === false) {
+    throw new Error(chalk.red(`Scenario file "${chalk.bold(pathOfScenario)}" not found`))
+  }
+
+  const scenarioFunction = require(pathOfScenario) as ScenarioFunction
 
   if (typeof scenarioFunction !== 'function') {
     throw new Error('Wrong scenario function')
